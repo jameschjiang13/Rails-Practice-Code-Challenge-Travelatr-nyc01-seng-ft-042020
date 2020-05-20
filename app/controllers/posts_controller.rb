4c3110c89c2ba1d1ae687293aed8497a8f083238
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-05-20 16:35:13 -0400

    final change

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,14 @@ end
 
 def create
     @post = Post.create(post_params)
-    redirect_to post_path(@post)
+
+    if @post.valid?
+        flash[:success]= "Awesome!"
+        redirect_to post_path(@post.id)
+    else
+        flash[:errors]= @post.errors.full_messages
+        redirect_to new_post_path
+    end
 end
 
 def edit
