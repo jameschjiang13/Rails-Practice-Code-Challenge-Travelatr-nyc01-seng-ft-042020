a68bb888770ce4519474b54eac3aae6661b8b107
jameschjiang13 <monochrome473@gmail.com>
2020-05-20 15:11:27 -0400

    15:11

---
+++ app/controllers/bloggers_controller.rb
@@ -0,0 +1,29 @@
+class BloggersController < ApplicationController
+    def index
+        @bloggers = Blogger.all
+    end
+    
+    def new
+    
+    end
+    
+    def create
+    
+    end
+    
+    def edit
+    
+    end
+    
+    def update
+    
+    end
+    
+    def show
+    
+    end
+    
+    def destroy
+    
+    end
+end
